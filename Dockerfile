7ffbe166812e0a04b883ee55f9ebc5a4e0d1b297
lucemia <lucemia@gmail.com>
2015-09-26 13:46:04 +0800

    update, still fail

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM asia.gcr.io/gothic-province-823/base
 MAINTAINER lucemia <davidchen@gliacloud.com>
 
 RUN apt-get install squid -y
+ADD squid.sh /home/squid.sh
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-docker build -t asia.gcr.io/gothic-province-823/squid . && gcloud docker push asia.gcr.io/gothic-province-823/squid
+docker build -t asia.gcr.io/gothic-province-823/squid . && \
+docker tag -f asia.gcr.io/gothic-province-823/squid squid && \
+gcloud docker push asia.gcr.io/gothic-province-823/squid && \
+kubectl delete pod squid && \
+kubectl create -f kube/pod.yaml
+kubectl get pod
